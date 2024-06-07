f82d1670f8fb41bdb0f6596579b3168ebc1c026e
Taufiq Pohan <m.taufiq30s@hotmail.com>
2024-06-08 00:00:01 +0700

    feat: Get Balance
    In this commit, i have implemented "Get Balance" feature in the Balance
    group according to official documentation.
    
    To accomplish it, i have added a method called get_with_params where the
    method is used if there are parameters in the GET operation.
    
    After that, i have installed 'serde_urlencoded', 'data-encoding', and
    'dotenv' where 'serde_urlencoded' is used to perform encoding of
    parameters. Then 'data-encoding' is used to encode Xendit API Key using
    base64 string following Basic Authentication Standart in RFC 7617.  And
    'dotenv' is used in dev-dependencies and used for testing.
    
    Signed-off-by: Taufiq Pohan <m.taufiq30s@hotmail.com>

---
+++ tests/balance.rs
@@ -0,0 +1,31 @@
+#[cfg(test)]
+mod tests {
+
+    use dotenv::dotenv;
+    use xendit::{balance_and_transaction::balance::{BalanceClient, GetBalanceParams}, client::XenditClient};
+
+    #[tokio::test]
+    async fn test_get_balance_default() {
+        dotenv().ok();
+        let client = XenditClient::new(std::env::var("XENDIT_API_KEY").unwrap_or("".to_string()));
+        let result = BalanceClient::new(&client)
+            .get_balance(
+                GetBalanceParams::new()
+            ).await;
+        assert_eq!(result, 13990471379);
+    }
+
+    #[tokio::test]
+    async fn test_get_balance_with_holding_account_type() {
+        dotenv().ok();
+        let client = XenditClient::new(std::env::var("XENDIT_API_KEY").unwrap_or("".to_string()));
+        let result = BalanceClient::new(&client)
+            .get_balance(
+                GetBalanceParams::new()
+                    .set_account_type("HOLDING")
+                    .set_currency("IDR")
+                    .build()
+            ).await;
+        assert_eq!(result, 100000);
+    }
+}
