dd548b6b303f51265666de4318686a586308d7ca
Taufiq Pohan <m.taufiq30s@hotmail.com>
2024-07-05 23:24:04 +0700

    fix: Missing properties in InvoiceResponse
    The field 'identity_amount' was missing from 'Bank' struct. This causes
    the deserialize process to fail. To solve this problem, add field
    'identity_amount' to 'Bank' struct.
    
    In addiction, change the data type of 'transfer_amount' to 'Option<f64>'
    because this field will be depracted soon.
    
    And update the unit test for invoice by clariflying the unit test
    function name and modifying the 'test_get_invoice' unit test by add code
    to create new invoice and retrieving it.
    
    Signed-off-by: Taufiq Pohan <m.taufiq30s@hotmail.com>

--- tests/invoice.rs
+++ tests/invoice.rs
@@ -5,10 +5,10 @@ mod test {
     use xendit::{client::XenditClient, invoice::{InvoiceBody, InvoiceClient}};
 
     #[tokio::test]
-    async fn test_create() {
+    async fn test_create_invoice() {
         dotenv().ok();
         let client = XenditClient::new(std::env::var("XENDIT_API_KEY").unwrap_or("".to_string()));
-        let request_body = InvoiceBody::new(String::from("63b7c72b9c6818001035db03"), 510000.0);
+        let request_body = InvoiceBody::new(String::from("63b7c72b9c6818001035db04"), 510000.0);
         assert!(
             match InvoiceClient::new(&client).create(request_body).await {
                 Ok(_) => true,
@@ -18,10 +18,13 @@ mod test {
     }
 
     #[tokio::test]
-    async fn test_get() {
+    async fn test_get_invoice() {
         dotenv().ok();
         let client = XenditClient::new(std::env::var("XENDIT_API_KEY").unwrap_or("".to_string()));
-        let invoice_id = String::from("667a94080d1dda6307187718");
+        let request_body = InvoiceBody::new(String::from("63b7c72b9c6818001035db03"), 510000.0);
+        let result = InvoiceClient::new(&client).create(request_body).await.expect("Failed Create Invoice");
+
+        let invoice_id = result.id;
         assert!(
             match InvoiceClient::new(&client).get(invoice_id).await {
                 Ok(_) => true,
@@ -31,7 +34,7 @@ mod test {
     }
 
     #[tokio::test]
-    async fn test_list() {
+    async fn test_get_list_of_invoices() {
         dotenv().ok();
         let client = XenditClient::new(std::env::var("XENDIT_API_KEY").unwrap_or("".to_string()));
         assert!(
