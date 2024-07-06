72bef9511d1d5734a854f7bc37c3d0598b237abd
Taufiq Pohan <m.taufiq30s@hotmail.com>
2024-07-06 12:03:50 +0700

    feat: Implement Payment Method API Client
    Create payment method API client to integrate with Payment Method
    resource from Xendit.
    
    Since this feature uses the latest Payment API, there are differences in
    the error response from the API. Update by adding 'path' and 'message'
    field to 'APIErrorDetail' struct and change all data type for
    'APIErrorResponse' and 'APIErrorDetail' to be optional, except the
    'message' field in 'APIErrorDetail'.
    
    Then there are endpoints that use PATCH method to update payment method
    and some POST endpoints that use parameters. Create function to handle
    PATCH method and a function to handle POST endpoint with parameters
    where that function will convert parameters to query parameter and send
    it to original POST function.
    
    And create unit test for test implementation of the Payment Method API
    Client.
    
    Signed-off-by: Taufiq Pohan <m.taufiq30s@hotmail.com>

---
+++ tests/payment_method.rs
@@ -0,0 +1,240 @@
+#[cfg(test)]
+mod tests {
+    use chrono::Duration;
+    use dotenv::dotenv;
+    use tokio::time::sleep;
+    use xendit::{client::XenditClient, payment_method::{payment_method_parameters::{EWalletParameter, PaymentType, Reusability, VirtualAccountParameter, VirtualAccountParameterUpdate}, GetPaymentMethodsParams, PaymentChannel::{EWallet, VirtualAccount}, PaymentMethodBody, PaymentMethodClient, PaymentMethodUpdateBody, PaymentStatus}};
+
+    fn initialize_client() -> XenditClient {
+        dotenv().ok();
+        XenditClient::new(std::env::var("XENDIT_API_KEY").unwrap())
+    }
+
+    #[tokio::test]
+    async fn create_payment_method_ewallet() {
+        let client = initialize_client();
+        let body = PaymentMethodBody::new(PaymentType::Ewallet, Reusability::OneTimeUse)
+            .set_description(String::from("Payment for order #123"))
+            .set_ewallet(EWalletParameter::new(
+                EWallet::Ovo)
+                .set_mobile_number(String::from("62817555493"))
+                .build())
+            .build();
+        assert!(
+            match PaymentMethodClient::new(&client).create_payment_method(body).await {
+                Ok(payment_method) => !payment_method.id.is_empty(),
+                Err(e) => {
+                    println!("{}", e);
+                    false
+                }
+            }
+        )
+    }
+
+    #[tokio::test]
+    async fn create_payment_method_va() {
+        let client = initialize_client();
+        let body = PaymentMethodBody::new(PaymentType::VirtualAccount, Reusability::MultipleUse)
+            .set_description(String::from("Payment for order #123"))
+            .set_virtual_account(VirtualAccountParameter::new(
+                VirtualAccount::Bri)
+                .set_amount(120000.0)
+                .set_customer_name(String::from("Airani Iofi"))
+                .set_expires_at(chrono::Utc::now() + Duration::days(2))
+                .build())
+            .build();
+        assert!(
+            match PaymentMethodClient::new(&client).create_payment_method(body).await {
+                Ok(payment_method) => !payment_method.id.is_empty(),
+                Err(e) => {
+                    println!("{}", e);
+                    false
+                }
+            }
+        )
+    }
+
+    #[tokio::test]
+    async fn get_payment_method_by_id() {
+        let client = initialize_client();
+        // Create a payment method first
+        let body = PaymentMethodBody::new(PaymentType::Ewallet, Reusability::OneTimeUse)
+            .set_description(String::from("Payment for order #123"))
+            .set_ewallet(EWalletParameter::new(
+                EWallet::Ovo)
+                .set_mobile_number(String::from("62817555493"))
+                .build())
+            .build();
+        let payment_method = PaymentMethodClient::new(&client).create_payment_method(body).await.unwrap();
+
+        // Get Payment Method by ID
+        assert!(
+            match PaymentMethodClient::new(&client).get_payment_method_by_id(&payment_method.id).await {
+                Ok(payment_method) => !payment_method.id.is_empty(),
+                Err(e) => {
+                    println!("{}", e);
+                    false
+                }
+            }
+        )
+    }
+
+    #[tokio::test]
+    async fn update_payment_method() {
+        let client = initialize_client();
+        // Create a payment method first
+        let body = PaymentMethodBody::new(PaymentType::VirtualAccount, Reusability::MultipleUse)
+            .set_description(String::from("Payment for order #1507"))
+            .set_virtual_account(VirtualAccountParameter::new(
+                VirtualAccount::Bri)
+                .set_amount(120000.0)
+                .set_customer_name(String::from("Airani Iofi"))
+                .set_expires_at(chrono::Utc::now() + Duration::days(2))
+                .build())
+            .build();
+        let payment_method = PaymentMethodClient::new(&client).create_payment_method(body).await.unwrap();
+        sleep(std::time::Duration::from_millis(500)).await;
+
+        // Update payment method
+        let updated_body = PaymentMethodUpdateBody::new()
+            .set_description(String::from("Updated Payment for order #1507"))
+            .set_virtual_account(VirtualAccountParameterUpdate::new()
+                .set_amount(150000.0)
+                .build())
+            .build();
+        assert!(
+            match PaymentMethodClient::new(&client).patch_payment_method(&payment_method.id, updated_body).await {
+                Ok(payment_method) => !payment_method.id.is_empty(),
+                Err(e) => {
+                    println!("{}", e);
+                    false
+                }
+            }
+        )
+    }
+
+    #[tokio::test]
+    async fn get_all_payment_methods() {
+        let client = initialize_client();
+        assert!(
+            match PaymentMethodClient::new(&client).get_all_payment_methods(None).await {
+                Ok(payment_methods) => !payment_methods.data.is_empty(),
+                Err(e) => {
+                    println!("{}", e);
+                    false
+                }
+            }
+        )
+    }
+
+    #[tokio::test]
+    async fn get_all_payment_methods_with_params() {
+        let client = initialize_client();
+        let params = GetPaymentMethodsParams::new()
+            .set_status(vec![
+                PaymentStatus::Pending,
+                PaymentStatus::Active
+            ])
+            .set_limit(5)
+            .build();
+        assert!(
+            match PaymentMethodClient::new(&client).get_all_payment_methods(Some(params)).await {
+                Ok(payment_methods) => !payment_methods.data.is_empty(),
+                Err(e) => {
+                    println!("{}", e);
+                    false
+                }
+            }
+        )
+    }
+
+    #[tokio::test]
+    async fn set_payment_method_expire() {
+        let client = initialize_client();
+        // Create a payment method first
+        let body = PaymentMethodBody::new(PaymentType::VirtualAccount, Reusability::MultipleUse)
+            .set_description(String::from("Payment for order #1507"))
+            .set_virtual_account(VirtualAccountParameter::new(
+                VirtualAccount::Bri)
+                .set_amount(120000.0)
+                .set_customer_name(String::from("Airani Iofi"))
+                .set_expires_at(chrono::Utc::now() + Duration::days(2))
+                .build())
+            .build();
+        let payment_method = PaymentMethodClient::new(&client).create_payment_method(body).await.unwrap();
+
+        // Expire payment method
+        assert!(
+            match PaymentMethodClient::new(&client).expire_payment_method(&payment_method.id, None).await {
+                Ok(payment_method) => !payment_method.id.is_empty(),
+                Err(e) => {
+                    println!("{}", e);
+                    false
+                }
+            }
+        )
+    }
+
+    #[tokio::test]
+    async fn get_payments_by_payment_method_id() {
+        let client = initialize_client();
+        let payment_method_client = PaymentMethodClient::new(&client);
+        // Create a payment method first
+        let body = PaymentMethodBody::new(PaymentType::VirtualAccount, Reusability::OneTimeUse)
+            .set_description(String::from("Payment for order #123"))
+            .set_virtual_account(VirtualAccountParameter::new(
+                VirtualAccount::Bri)
+                .set_amount(120000.0)
+                .set_customer_name(String::from("Airani Iofi"))
+                .set_expires_at(chrono::Utc::now() + Duration::days(2))
+                .build())
+            .build();
+        let payment_method = payment_method_client.create_payment_method(body).await.unwrap();
+        sleep(std::time::Duration::from_millis(500)).await;
+
+        // Simulate payment (set payment was success)
+        let status = payment_method_client.simulate_payment(&payment_method.id, 120000.0).await.unwrap();
+        println!("status {}; message {}", status.status, status.message);
+        sleep(std::time::Duration::from_secs(1)).await;
+
+        // Execute
+        assert!(
+            match payment_method_client.get_payments_by_payment_method_id("pm-e64c59c3-8f25-4e70-abc1-cc2d1973780d".to_string(), None).await {
+                Ok(payment) => !payment.data.is_empty(),
+                Err(e) => {
+                    println!("{}", e);
+                    false
+                }
+            }
+        )
+    }
+
+    // TODO: Create Customer Client to use this unit test
+    // #[tokio::test]
+    // // This endpoint only applies to BRI Direct Debit.
+    // async fn auth_otp() {
+    //     let client = initialize_client();
+    //     // Create a payment method first
+    //     let body = PaymentMethodBody::new(PaymentType::DirectDebit, Reusability::OneTimeUse)
+    //         .set_description(String::from("Payment for order #123"))
+    //         .set_direct_debit(DirectDebitParameter::new(
+    //             DirectDebit::Bri)
+    //             .set_require_auth(true)
+    //             .set_mobile_number(String::from("+62817555493"))
+    //             .build()
+    //         )
+    //         .set_customer_id("cust-239c16f4-866d-43e8-9341-7badafbc019f".to_string()).build();
+    //     let payment_method = PaymentMethodClient::new(&client).create_payment_method(body).await.unwrap();
+
+    //     // Execute
+    //     assert!(
+    //         match PaymentMethodClient::new(&client).auth_payment_method(&payment_method.id, "123456").await {
+    //             Ok(payment_method) => !payment_method.id.is_empty(),
+    //             Err(e) => {
+    //                 println!("{}", e);
+    //                 false
+    //             }
+    //         }
+    //     )
+    // }
+}
